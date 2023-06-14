de618d4ff622fbd3e0d4b92dd247aec289c05b9f
fernandogtorrez <fernandogtorrez@gmail.com>
2023-06-14 15:02:08 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const res = require("express/lib/response");
+const path = require('path')
+const app = express();
+const PORT = 3030
+
+
+/* configuraciones */
+app.use(express.static('public'))
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/home.html'))
+})
+app.get('/about',(req,res)=> res.sendFile(path.join(__dirname,'/views/about.html')))
+app.get('/contact',(req,res)=> res.sendFile(path.join(__dirname,'/views/contact.html')))
+app.get('/music',(req,res)=> res.sendFile(path.join(__dirname,'/views/music.html')))
+app.get('/*',(req,res)=> res.sendFile(path.join(__dirname,'/views/404.html')))
+
+app.listen(PORT, () => {
+  console.log(`Hola bienvenidos al puerto ${PORT}`);
+});
